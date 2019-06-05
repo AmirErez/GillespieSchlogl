cb74fcdeb176df72e6d06fb17c40a2a03a4dda4b
Amir Erez <aerez@della5.princeton.edu>
2019-06-05 16:55:58 -0400

    Fixed

--- CollectQuenches.m
+++ CollectQuenches.m
@@ -1,6 +1,7 @@
 % Collects Gillespie output in folder and makes table with params and corr time
 
-all_collect = {'quench_1cell'};
+%all_collect = {'quench_1cell'};
+all_collect = {'quench_NS'};
 
 for aa=1:length(all_collect)
     collectdir = all_collect{aa};
@@ -14,11 +15,11 @@ for aa=1:length(all_collect)
         loaded = load([collectdir filesep d(ff).name]);
         
         tempstruct = struct;
-        tempstruct.theta_i = loaded.theta_i;
-        tempstruct.theta_f = loaded.theta_f;
-        tempstruct.h_i = loaded.h_i;
-        tempstruct.h_f = loaded.h_f;
-        tempstruct.nc = loaded.nc;
+        tempstruct.theta_i = loaded.Ising_i.theta;
+        tempstruct.theta_f = loaded.Ising_f.theta;
+        tempstruct.h_i = loaded.Ising_i.h;
+        tempstruct.h_f = loaded.Ising_f.h;
+        tempstruct.nc = loaded.Ising_i.nc;
         tempstruct.n_replicates = loaded.n_replicates;
         tempstruct.t_start = loaded.tspan(1);
         tempstruct.t_end = loaded.tspan(2);
